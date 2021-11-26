d39406727520f4eedf343b37a19094180aef55da
Sofya Ghazaryan <94921668+sofya0711@users.noreply.github.com>
2021-11-26 13:18:53 +0300

    Delete src/Homeworks directory

--- src/Homeworks/Array/DynamicArray/Jnjelu.java
+++
@@ -1,8 +0,0 @@
-package Homeworks.Array.DynamicArray;
-
-public class Jnjelu {
-    public static void main(String[] args) {
-        Boolean b = new Boolean("Hello");
-        System.out.println(b);
-    }
-}
